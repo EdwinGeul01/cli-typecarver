00691ac4f5f21234375f4cf572d6cbd6e2f65984
EdwinGeul01 <edwingeul01@gmail.com>
2025-06-21 22:59:26 -0600

    feat: first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# CLI Tool to generate TypeScript interfaces from a MySQL database schema.
+
+<img src="./typecaver.png" width="230" alt="TypeCaver Logo">
+
+- Tool created using the lib [database-to-interfaces](https://github.com/EdwinGeul01/database-to-interfaces-new) to generate TypeScript interfaces from a MySQL database schema.
+
+
+## How to use 
+1. install the dependencies globally:
+
+   ```bash
+   npm install -g database-to-interfaces
+   ```
+
+2. create a configuration file `configDatabase.config` in the root of your project with the following content:
+
+   ```yaml
+    port: 3306
+    host: localhost
+    user: root
+    password: 1234
+    database: midb 
+    filename: ./interfacesdb.ts
+    filepath: ./a/
+   ```
+
+3. run the tool:
+
+   ```bash
+   npx database-to-interfaces
+   ```
+
+
+## Configuration file | content 🧾
+- `port`: the port of the MySQL server (default: 3306)
+- `host`: the host of the MySQL server (default: localhost)
+- `user`: the user to connect to the MySQL server (default: root)
+- `password`: the password to connect to the MySQL server (default: empty)
+- `database`: the name of the database to connect to (default: empty)
+- `filename`: the name of the file where the interfaces will be generated (default: interfaces)
+- `filepath`: the path where the file will be generated (default: ./)
+- `prefix`: the prefix to add to the interfaces (default: empty) , for example 'DB_' will generate interfaces like `DB_User`, `DB_Product`, etc.
+- `dbType`: the type of database to connect to (default: mysql) , currently only supports `mysql` and `postgresql`.
+
+
+## Help ⛑️
+-  you can also use the template file `configDatabase.config.template` to create your own configuration file.,  just remove the `.template` extension and fill in the values.
+
---
+++ index.ts
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+
+import {
+  convertTablesToTypescriptInterface,
+  initModules,
+  saveInterfaceFile,
+} from "database-to-interfaces";
+import { readFile } from "fs/promises";
+
+async function main() {
+  console.log(`
+▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+██ ▄▄▀█ ▄▄▀█▄▄ ▄▄█ ▄▄▀██ ▄▄▀█ ▄▄▀██ ▄▄▄ ██ ▄▄▄███▄▄ ▄▄██ ▄▄▄ ███▄ ▄██ ▀██ █▄▄ ▄▄██ ▄▄▄██ ▄▄▀██ ▄▄▄█ ▄▄▀██ ▄▄▀██ ▄▄▄██
+██ ██ █ ▀▀ ███ ███ ▀▀ ██ ▄▄▀█ ▀▀ ██▄▄▄▀▀██ ▄▄▄█████ ████ ███ ████ ███ █ █ ███ ████ ▄▄▄██ ▀▀▄██ ▄▄██ ▀▀ ██ █████ ▄▄▄██
+██ ▀▀ █ ██ ███ ███ ██ ██ ▀▀ █ ██ ██ ▀▀▀ ██ ▀▀▀█████ ████ ▀▀▀ ███▀ ▀██ ██▄ ███ ████ ▀▀▀██ ██ ██ ████ ██ ██ ▀▀▄██ ▀▀▀██
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+[CLI Database to Interface Generator]
+[Version 0.0.1]
+[Author: Edge1212]
+
+`);
+
+  //leer cada linea del archivo de configuracion
+  let configMap;
+  try {
+    configMap = await readConfigFile();
+  } catch (error) {}
+
+  if (!configMap || configMap.size === 0) {
+    console.error("No configuration found 🔍. Please check your config file. ");
+    return;
+  }
+
+  console.log("Configuration loaded successfully ✅");
+  if (configMap.get("dbType") === undefined) {
+    console.log("No database type specified 🚧 Using default: mysql ...");
+  }
+
+  let DBModuleToUse;
+
+  DBModuleToUse = initModules(configMap.get("dbType") || "mysql", {
+    host: configMap.get("host") || "localhost",
+    port: parseInt(configMap.get("port") || "3306"),
+    database: configMap.get("database") || "test",
+    user: configMap.get("user") || "root",
+    password: configMap.get("password") || "",
+    schema: configMap.get("schema") || "",
+  });
+
+  //test connection
+
+  const connectionSuccess = await DBModuleToUse.testConnection();
+  if (!connectionSuccess) {
+    console.error(
+      "Connection failed ❌. Please check your database credentials."
+    );
+    return;
+  }
+
+  const r = await DBModuleToUse.getTablesMapWithColumns();
+  const s = convertTablesToTypescriptInterface(
+    r,
+    configMap.get("prefix") || ""
+  );
+
+  await saveInterfaceFile(s, {
+    fileName: configMap.get("filename") || "interfaces.ts",
+    filePath: configMap.get("filepath") || "./",
+  });
+
+  console.log(`Interfaces file generated successfully! 🎉 `);
+}
+
+main();
+
+async function readConfigFile() {
+  const configFile = await readFile("./configDatabase.config", "utf-8");
+  const configLines = configFile
+    .split("\n")
+    .map((line) => line.trim().replace(" ", ""))
+    .filter((line) => line && !line.startsWith("#"));
+
+  const configMap = new Map<string, string>();
+  configLines.forEach((line) => {
+    const [key, value] = line.split(":");
+    if (key && value) {
+      configMap.set(key, value);
+    }
+  });
+  return configMap;
+}
